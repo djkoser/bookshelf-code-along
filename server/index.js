f55c27bb60a6daf38342b1c4ef783797ac01db26
djkoser <koser.david@gmail.com>
2021-03-10 13:36:28 -0600

    Complete codealong

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express(); 
+const path = require('path')
 const port = 4000;
 
 const bc = require('./controllers/books_controller')
 
-app.listen(port, () => console.log(`Server listening on port ${port}.`));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'/../build')))
 
 app.get('/api/books', bc.read); 
 app.post('/api/books', bc.create);
 app.put('/api/books/:id', bc.update);
 app.delete('/api/books/:id', bc.delete);
+
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
